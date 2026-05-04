8e8ab80e0f2193e3f874eb59ac0e7af1227c99d4
sonyavaKP2317 <sofvat5@gmail.com>
2026-05-04 19:49:28 +0000

    Добавление комментариев и создание скрипта развертывания

---
+++ deploy.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "Запуск приложения Task Tracker"
+echo ">>Установка Django>>"
+pip install django
+echo "Программа готова к использованию!"
+echo "Для запуска перейдите во вкладку Preview и нажмите на кнопку Run"
