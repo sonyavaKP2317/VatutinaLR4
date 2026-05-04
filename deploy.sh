8ae407fa65b6da71958e51fc4554890d382fff00
sonyavaKP2317 <sofvat5@gmail.com>
2026-05-04 20:03:08 +0000

    Изменение скрипт развертывания после проверки Нерсисян А.С.

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,7 @@
 echo "Запуск приложения Task Tracker"
 echo ">>Установка Django>>"
 pip install django
+echo ">>Подгатавливаю служебные таблицы>>"
+python manage.py migrate
 echo "Программа готова к использованию!"
 echo "Для запуска перейдите во вкладку Preview и нажмите на кнопку Run"
