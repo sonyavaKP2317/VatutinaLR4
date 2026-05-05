38b28203466d567389f445054b97a71eee004f3a
sonyavaKP2317 <sofvat5@gmail.com>
2026-05-05 19:46:53 +0000

    Отредактировала скрипт развертывания

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 echo "Запуск приложения Task Tracker"
-echo ">>Установка Django>>"
-pip install django
 echo ">>Подгатавливаю служебные таблицы>>"
 python manage.py migrate
+echo ">>Проверяю корректность проекта>>"
+python manage.py check
 echo "Программа готова к использованию!"
 echo "Для запуска перейдите во вкладку Preview и нажмите на кнопку Run"
